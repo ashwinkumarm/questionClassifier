cc9c56f6972488669d7343e0a15eae133e260bc4
ashwinkumarm <ashbushster@gmail.com>
2018-04-24 00:58:43 -0500

    First commit

---
+++ questionClassifier.py
@@ -0,0 +1,8 @@
+'''
+Created on 23-Apr-2018
+
+@author: Ashwin
+'''
+
+## This projects tries to reproduce the work of li and roth. They have tried hierarchial classifier
+## and they have also mentioned in their paper that, they were not able to find any 
