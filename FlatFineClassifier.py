f75efa81248e6ae892fa95c6b33800247ed18fa1
ashwinkumarm <ashbushster@gmail.com>
2018-04-25 15:21:21 -0500

    fineclassfier heirar

--- FlatFineClassifier.py
+++ FlatFineClassifier.py
@@ -8,218 +8,206 @@ from practnlptools.tools import Annotator
 from readproperties import read_property
 import naiveBayes
 
+# #removing special characters from sentence##
 
-
-##removing special characters from sentence##
 
 def preprocess(raw_sentence):
-    sentence= re.sub(r'[$|.|!|"|(|)|,|;|`|\']',r'',raw_sentence)
+    sentence = re.sub(r'[$|.|!|"|(|)|,|;|`|\']', r'', raw_sentence)
     return sentence
 
-##making the file format ready to use##
+# #making the file format ready to use##
+
 
 def file_preprocess(filename):
-    corpus=[]
-    classes=[]
-    f=open(filename,'r')
-    lines=f.readlines()
+    corpus = []
+    classes = []
+    f = open(filename, 'r')
+    lines = f.readlines()
     for line in lines:
-        line=line.rstrip('\n')
-        if not (line=="\n"):
+        line = line.rstrip('\n')
+        if not (line == "\n"):
             classes.append((line.split()[0]).split(":")[1])
     for line in lines:
-        line=line.rstrip('\n')
-        line=preprocess(line)
-        sentence=""
-        words=line.split()
-        for i in range(0,len(words)):
-            if not(i==0):
-                sentence=sentence+(words[i])+" "
+        line = line.rstrip('\n')
+        line = preprocess(line)
+        sentence = ""
+        words = line.split()
+        for i in range(0, len(words)):
+            if not(i == 0):
+                sentence = sentence + (words[i]) + " "
         corpus.append(sentence)
     f.close()
-    return corpus,classes
+    return corpus, classes
 
+# #Compute POS##
 
-##Compute POS##
 
 def compute_POS_Tags(corpus):
-    POS=[]
-    f=open(read_property('POS_features_test_coarse_path'),"r")
-    corpus=[]
+    POS = []
+    f = open(read_property('POS_features_test_coarse_path'), "r")
+    corpus = []
     for lines in f:
-        l=lines.split()
-        words=""
+        l = lines.split()
+        words = ""
         for w in l:
-            words=words+w+" "
+            words = words + w + " "
         POS.append(words)   
     return POS         
+      
+# #Compute NER## 
 
-      
-      
-##Compute NER## 
     
 def compute_NER(corpus):
-    NER=[]
-    f=open(read_property('NER_features_test_coarse_path'),"r")
+    NER = []
+    f = open(read_property('NER_features_test_coarse_path'), "r")
     for lines in f:
-        l=lines.split()
-        words=""
+        l = lines.split()
+        words = ""
         for w in l:
-            words=words+w+" "
+            words = words + w + " "
         NER.append(words)        
     return NER
 
+# #Compute Chunks##  
 
-
-##Compute Chunks##  
    
 def compute_Chunks(corpus):
-    f=open(read_property('Chunk_features_test_path'),"r")
-    chunks=[]
+    f = open(read_property('Chunk_features_test_path'), "r")
+    chunks = []
     for lines in f:
-        l=lines.split()
-        words=""
+        l = lines.split()
+        words = ""
         for w in l:
-            words=words+w+" "
+            words = words + w + " "
         chunks.append(words)  
     return chunks      
 
+
 def compute_REL_Tags(corpus):
-    POS=[]
-    f=open(read_property('REL_features_test_coarse_path'),"r")
-    corpus=[]
+    POS = []
+    f = open(read_property('REL_features_test_coarse_path'), "r")
+    corpus = []
     for lines in f:
-        l=lines.split()
-        words=""
+        l = lines.split()
+        words = ""
         for w in l:
-            words=words+w+" "
+            words = words + w + " "
         POS.append(words)   
     return POS      
-
 
 ######################################TRAINING############################################
 
 #######Train class labels#####
 
-train_class=[]
-f=open(read_property('trainingfilepath'),'r')
-lines=f.readlines()
-for line in lines:
-    line=line.rstrip('\n')
-    if not (line=="\n"):
-        train_class.append((line.split()[0]).split(":")[1])
 
+train_class = []
+f = open(read_property('trainingfilepath'), 'r')
+lines = f.readlines()
+for line in lines:
+    line = line.rstrip('\n')
+    if not (line == "\n"):
+        train_class.append((line.split()[0]).split(":")[1])
 
 ###words in question###
 
 print "Training"
-f=open(read_property('word_features_train_coarse_path'),"r")
-corpus=[]
+f = open(read_property('word_features_train_coarse_path'), "r")
+corpus = []
 for lines in f:
-    l=lines.split()
-    words=""
+    l = lines.split()
+    words = ""
     for w in l:
-        words=words+w+" "
+        words = words + w + " "
     corpus.append(words)        
-vectorizer_words= CountVectorizer(min_df=1)
+vectorizer_words = CountVectorizer(min_df=1)
 X_words = vectorizer_words.fit_transform(corpus)
 f.close()
 print "word feature extraction done"
 
-
-
 ###POS tags in question###
 
-f=open(read_property('POS_features_train_coarse_path'),"r")
-corpus=[]
+f = open(read_property('POS_features_train_coarse_path'), "r")
+corpus = []
 for lines in f:
-    l=lines.split()
-    words=""
+    l = lines.split()
+    words = ""
     for w in l:
-        words=words+w+" "
+        words = words + w + " "
     corpus.append(words)        
-vectorizer_POS= CountVectorizer(min_df=1)
+vectorizer_POS = CountVectorizer(min_df=1)
 X_POS = vectorizer_POS.fit_transform((corpus))  
 f.close()
 print "POS feature extraction done"
 
-
 ###NER tags in question###
 
-f=open(read_property('NER_features_train_coarse_path'),"r")
-corpus=[]
+f = open(read_property('NER_features_train_coarse_path'), "r")
+corpus = []
 for lines in f:
-    l=lines.split()
-    words=""
+    l = lines.split()
+    words = ""
     for w in l:
-        words=words+w+" "
+        words = words + w + " "
     corpus.append(words)        
-vectorizer_NER= CountVectorizer(min_df=1)
+vectorizer_NER = CountVectorizer(min_df=1)
 X_NER = vectorizer_NER.fit_transform((corpus))  
 print "Vectorize"
 f.close()
 print "NER feature extraction done"
 
-
 ###Chunk tags in question###
 
-f=open(read_property('Chunk_features_train_path'),"r")
-corpus=[]
+f = open(read_property('Chunk_features_train_path'), "r")
+corpus = []
 for lines in f:
-    l=lines.split()
-    words=""
+    l = lines.split()
+    words = ""
     for w in l:
-        words=words+w+" "
+        words = words + w + " "
     corpus.append(words)        
-vectorizer_Chunk= CountVectorizer(min_df=1)
+vectorizer_Chunk = CountVectorizer(min_df=1)
 X_Chunk = vectorizer_Chunk.fit_transform((corpus))  
 f.close()
 print "Chunk feature extraction done"
 
-
-f=open(read_property('REL_features_train_coarse_path'),"r")
-corpus=[]
+f = open(read_property('REL_features_train_coarse_path'), "r")
+corpus = []
 for lines in f:
-    l=lines.split()
-    words=""
+    l = lines.split()
+    words = ""
     for w in l:
-        words=words+w+" "
+        words = words + w + " "
     corpus.append(words)        
-vectorizer_REL= CountVectorizer(min_df=1)
+vectorizer_REL = CountVectorizer(min_df=1)
 X_REL = vectorizer_REL.fit_transform((corpus))  
 f.close()
 print "REL feature extraction done"
 
-
-X=hstack((X_words,X_POS))
+X = hstack((X_words, X_POS))
 X_intermediate = hstack((X, X_REL))
-X_train=hstack((X_intermediate,X_NER))
-X_train=hstack((X_train,X_Chunk))
-
+X_train = hstack((X_intermediate, X_NER))
+X_train = hstack((X_train, X_Chunk))
 
 ######################################TESTING############################################
 
 print "In Testing"
-filename_test=read_property('testfilepath')
-corpus_test,test_class_gold=file_preprocess(filename_test)
+filename_test = read_property('testfilepath')
+corpus_test, test_class_gold = file_preprocess(filename_test)
 
 ###words in question###
 
 X_words = vectorizer_words.transform(corpus_test)
 print "Word features extracted"
 
-
 ###POS tags in question###
 
 X_POS = vectorizer_POS.transform(compute_POS_Tags(corpus_test))     
 print "POS features extracted"
 
-
 ###NER tags in question###
 
 X_NER = vectorizer_NER.transform(compute_NER(corpus_test))  
 print "NER features extracted" 
-
 
 ###Chunk tags in question###
 
@@ -229,12 +217,10 @@ print "Chunk features extracted"
 X_REL = vectorizer_REL.transform(compute_REL_Tags(corpus_test))
 print "REL features extracted"
 
-
-X=hstack((X_words,X_POS))
+X = hstack((X_words, X_POS))
 X_intermediate = hstack((X, X_REL))
-X_test=hstack((X_intermediate,X_NER))
-X_test=hstack((X_test,X_Chunk))
-
+X_test = hstack((X_intermediate, X_NER))
+X_test = hstack((X_test, X_Chunk))
 
 ###################Applying the LinearSVC Classifier#########################
 
--- README.txt
+++ README.txt
@@ -1,5 +1,13 @@
-1)python extractclassestrainingtext.py
-2)python Training_Coarse.py
-3)python Training_Coarse_Classification.py
-4)python Training_Fine.py
-5)Training_Fine_Classification.py
+The program finds the accuracy for coarse and fine classifier for TREC data set. We find accuracy
+for single prediction and multiple classes where classes are less than 5. 
+
+Each program takes several hours to run. So we saved everything in files. 
+
+extractclasstrainingtext - extracts coarse classifier and save in different files
+naiveBayes - our model to predict top 5 classes
+RelWord - TO extract the Relational words
+Training_coarse - it computes and stores NER,POS,CHUNKS,WORDS and saves for training data set.
+Training_coarse_classificatoin - Reads the training classified and predicts for test and stores and prints the accuracy for coarse classification
+Training_fine - Extracts data for fine
+Training_fine_Classification - Reads the training and predicts fine labels and stores the output and prints accuracy 
+FlatFineClassifier - For classifying fine, implemented flat architecture instead of hierarchial
